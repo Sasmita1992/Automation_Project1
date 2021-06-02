9aac449153dd160e42e0a026fb724b329fa56e76
root <root@ip-172-31-57-38.ec2.internal>
2021-06-02 10:29:57 +0000

    This is the final code for submission

--- automation.sh
+++ automation.sh
@@ -24,3 +24,26 @@ aws s3 \
 cp /tmp/${myname}-httpd-logs-${timestamp}.tar \s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
 #Creating Inventory.html iand apend all the data every time#
 size=$( (du -k /tmp/${myname}-httpd-logs-${timestamp}.tar | cut -f1) )
+######################TASK 3 STARTS FROM HERE################################
+#Creating Inventory.html iand apend all the data every time#
+size=$( (du -h /tmp/${myname}-httpd-logs-${timestamp}.tar | cut -f1) )
+FILE='/var/www/html/inventory.html'
+if [ ! -e $FILE ]; then
+
+   sudo touch /var/www/html/inventory.html
+
+   sudo echo "<h3>Log Type&emsp;&emsp;Time Created&emsp;&emsp;Type&emsp;&emsp;Size</h3>"  > /var/www/html/inventory.html
+
+   sudo echo  "$value<p>-httpd-log&emsp;&emsp;&emsp;$timestamp&emsp;&emsp;&emsp;tar&emsp;&emsp;&emsp;$size</p>" >> /var/www/html/inventory.html
+   cd /root/Automation_Project
+else
+    sudo echo "<p>-httpd-log&emsp;&emsp;&emsp;$timestamp&emsp;&emsp;&emsp;tar&emsp;&emsp;&emsp;$size</p>"  >> /var/www/html/inventory.html
+fi
+#Submit Cron job#
+FILE1=/etc/cron.d/automation
+if [ ! -e $FILE1 ]; then
+
+        cd /etc/cron.d/
+	sudo touch /etc/cron.d/automation
+	sudo echo "59 23 * * * root /root/Automation_Project/automation.sh" > /etc/cron.d/automation
+fi
