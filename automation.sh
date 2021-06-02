4426f092900d6968b4a53229a756ebcfe827cdc7
root <root@ip-172-31-57-38.ec2.internal>
2021-06-02 09:41:56 +0000

    Task 2 completed

---
+++ automation.sh
@@ -0,0 +1,25 @@
+###########################This is for TASK 2#######################
+#Update package#
+sudo apt update -y
+#Check If Apache2 is installed or not#
+dpkg --get-selections | grep apache2
+#Install Pacage apache2#
+sudo apt install apache2
+#Check for apache2 status#
+service apache2 status
+#Restart apache2 Service#
+sudo systemctl start apache2
+# Installing awscli
+sudo apt update
+sudo apt install awscli
+#Code for transfering the tar log file to S3#
+cd /var/log/
+myname='sasmita'
+timestamp=$(date '+%d%m%Y-%H%M%S')
+sudo tar -cvf ${myname}-httpd-logs-${timestamp}.tar --exclude=other_vhosts_access.log  apache2/*.log 
+cp /var/log/${myname}-httpd-logs-${timestamp}.tar /tmp 
+s3_bucket=upgrad-sasmita
+aws s3 \
+cp /tmp/${myname}-httpd-logs-${timestamp}.tar \s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+#Creating Inventory.html iand apend all the data every time#
+size=$( (du -k /tmp/${myname}-httpd-logs-${timestamp}.tar | cut -f1) )
